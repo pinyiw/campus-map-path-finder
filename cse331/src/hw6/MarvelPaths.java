5d0e5ca456eecb52a4522d87f3f14d7703ce8908
pinyiw <pinyiw@bcrl.csepclab.cs.washington.edu>
2016-05-10 17:19:42 -0700

    Building hw6 MarvelPaths

--- cse331/src/hw6/MarvelPaths.java
+++ cse331/src/hw6/MarvelPaths.java
@@ -1,6 +1,7 @@
 package hw6;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -12,6 +13,7 @@ import java.util.Set;
 
 import hw5.Graph;
 import hw5.GraphNode;
+import hw6.MarvelParser.MalformedDataException;
 
 
 /**
@@ -30,16 +32,17 @@ public class MarvelPaths {
 	// Representation invariant for every MarvelPaths m:
 	//	
 	
-	public MarvelPaths(String fileName) {
+	/** 
+	 * @param fileName the file name of the tsv data to be process.
+	 * @throws MalformedDataException if the file of the given file name has
+	 * 		   invalid tsv format.
+	 * @effects Constructs a new MarvelPaths with the given file name and build
+	 * 			a graph from that file.
+	 */
+	public MarvelPaths(String fileName) throws MalformedDataException {
 		Set<String> characters = new HashSet<String>();
 		Map<String, List<String>> books = new HashMap<String, List<String>>();
-		try {
-			MarvelParser.parseData(fileName, characters, books);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.err.println(e.toString());
-			e.printStackTrace(System.err);
-		}
+		MarvelParser.parseData(fileName, characters, books);
 		List<GraphNode> chars = new ArrayList<GraphNode>();
 		for (String character: characters) {
 			chars.add(new GraphNode(character));
@@ -59,11 +62,22 @@ public class MarvelPaths {
 	}
 	
 	/**
+	 * Find the shortest path between two characters, returning the characters 
+	 * visited and books that connected those characters in the path.
 	 * 
-	 * @param char1
-	 * @param char2
-	 * @return an empty list of string if connection between char1 and char2 was not found, otherwise
-	 * 		   a list of string .......
+	 * @param char1 the character we want to start on.
+	 * @param char2 the character we want to find path from char1.
+	 * @return null if connection between char1 and char2 was not found, 
+	 * 		   an empty list of string if char1 == char2,
+	 * 		   otherwise, return a list of string with each characters followed
+	 * 		   by the book that connect current character to the previous one.
+	 * 		   If more than one path are available, it would return the
+	 * 		   shortest path with lexicographically first character at each
+	 * 		   step in the path, and lexicographically lowest title book. 
+	 * 		   For example, char1 to charA via book1
+	 * 						charA to charB via book2
+	 * 						charB to char2 via book3,
+	 * 		   It would return: [charA, book1, charB, book2, char2, book3].
 	 */
 	public List<String> search(String char1, String char2) {
 		GraphNode start = new GraphNode(char1);
@@ -103,22 +117,51 @@ public class MarvelPaths {
 		return processPathToReadableList(resultPath);
 	}
 	
+	/**
+	 * Process the given list of GraphNode to a readable list of string.
+	 * 
+	 * @param path the list of GraphNode to be processed.
+	 * @return null if path is empty,
+	 * 		   an empty list of string if the first and last GraphNode equals,
+	 * 		   otherwise, return a list of string with each characters followed
+	 * 		   by the book that connect current character to the previous one.
+	 * 		   If more than one path are available, it would return the
+	 * 		   shortest path with lexicographically first character at each
+	 * 		   step in the path, and lexicographically lowest title book. 
+	 * 		   For example, char1 to charA via book1
+	 * 						charA to charB via book2
+	 * 						charB to char2 via book3,
+	 * 		   It would return: [charA, book1, charB, book2, char2, book3].
+	 */
 	private List<String> processPathToReadableList(List<GraphNode> path) {
-		List<String> result = new ArrayList<String>();
-		if (!path.isEmpty()) {
+		if (path.isEmpty()) {
+			return null;
+		} else if (path.get(0).equals(path.get(path.size() - 1))) {
+			return new ArrayList<String>();
+		} else {
+			List<String> result = new ArrayList<String>();
 			for (int i = 0; i < path.size() - 1; i++) {
 				result.add(path.get(i + 1).getName());
 				List<String> edges = graph.getEdgeData(path.get(i), 
 														path.get(i + 1));
-				
+				Collections.sort(edges);
+				result.add(edges.get(0));
 			}
+			return result;
 		}
-		return result;
 	}
 	
-	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in);
+	public static void main(String[] args) throws MalformedDataException {
 		MarvelPaths mp = new MarvelPaths("src/hw6/data/staffSuperheroes.tsv");
+		// test
+		Set<GraphNode> chars = mp.graph.nodes();
+		for (GraphNode node: chars) {
+			System.out.println(node.getName());
+		}
+		
+		System.out.println();
+		//	
+		Scanner console = new Scanner(System.in);
 		boolean again = true;
 		while (again) {
 			System.out.println("What connection of characters are you interested in?");
@@ -129,6 +172,7 @@ public class MarvelPaths {
 			
 			List<String> path = mp.search(char1, char2);
 			// print out the path it returns
+			System.out.println(path);
 			
 			System.out.print("Want to search more? (Y/N) ");
 			String ans = console.nextLine();
