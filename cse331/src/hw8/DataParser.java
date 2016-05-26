2c1d3b1132aa90c08f0006386dca0686becc7d2d
pinyiw <pinyiw@divy.csepclab.cs.washington.edu>
2016-05-25 18:20:08 -0700

    Implement hw8 data parser

---
+++ cse331/src/hw8/DataParser.java
@@ -0,0 +1,170 @@
+package hw8;
+import java.io.*;
+import java.util.*;
+
+import javafx.util.Pair;
+
+/**
+ * Parser utility to load the UW map dataset.
+ */
+public class DataParser {
+	/**
+	 * A checked exception class for bad data files
+	 */
+	@SuppressWarnings("serial")
+	public static class MalformedDataException extends Exception {
+		public MalformedDataException() {}
+		
+		public MalformedDataException(String message) {
+			super(message);
+		}
+		
+		public MalformedDataException(Throwable cause) {
+			super(cause);
+		}
+		
+		public MalformedDataException(String message, Throwable cause) {
+			super(message, cause);
+		}
+	}
+	
+	/**
+	 * Reads the campus building dataset.
+	 * Each line of the input file contains a short name, a long name, an x and
+	 * a y of a place, separated by a tab character.
+	 * 
+	 * @requires fileName is a valid file path.
+	 * @param fileName the file that will be read.
+	 * @param buildings the set that all buildings will be stored, typically
+	 * 		  empty when the routine is called.
+	 * @modifies buildings
+	 * @effects fills locations into buildings.
+	 * @throws MalformedDataException if  the file is not well-formed:
+	 * 			each line contains exactly four tokens separated by a tab.
+	 */
+	public static void parseBuildingsData(String fileName, 
+			Set<Location> buildings) throws MalformedDataException {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+			
+			String inputLine;
+			while ((inputLine = reader.readLine()) != null) {
+				
+				if (inputLine.length() == 0) {
+					continue;
+				}
+				
+				String[] tokens = inputLine.split("\t");
+				if (tokens.length != 4) {
+					throw new MalformedDataException("Line should contain exactly"
+							+ "three tab:" + inputLine);
+				}
+				
+				String shortName = tokens[0];
+				String longName = tokens[1];
+				String x = tokens[2];
+				String y = tokens[3];
+				
+				if (shortName != null && shortName.length() != 0) {
+					buildings.add(new Location(shortName, longName, x, y));
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e.toString());
+			e.printStackTrace(System.err);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					System.err.println(e.toString());
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Reads the campus paths dataset.
+	 * The input file contains a straight-line segments of walking paths. For
+	 * each endpoint of a path segment, there is a line in the file listing the
+	 * pixel coordinates of that point, followed by a tab-indented line for
+	 * each endpoint to which it is connected with a path segment.
+	 * Each indented line lists the coordinates of the other endpoint and the
+	 * distance of the segment between them in feet.
+	 * 
+	 * @requires fileName is a valid file path.
+	 * @param fileName the file that will be read.
+	 * @param paths the map in which all paths will be stored, typically empty
+	 * 		  the routine is called.
+	 * @modifies paths
+	 * @effects fills the map, paths, with key starting point of a path with a
+	 * 			Pair storing x and y and its name and value. While the value of
+	 * 			paths is a Pair that stores another Pair of x and y as name and
+	 * 			a Double of distance as value.
+	 * @throws MalformedDataException if the file is not well-formed as
+	 * 		   described in the method comments.
+	 */
+	public static void parsePathsData(String fileName,
+			Map<Pair<String, String>, 
+				List<Pair<Pair<String, String>, Double>>> paths)
+			throws MalformedDataException {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+			
+			String first = reader.readLine();
+			while (first!= null) {
+				String[] xy = first.split(",");
+				if (xy.length != 2) {
+					throw new MalformedDataException("Line should have two"
+							+ "coordinates: " + first);
+				}
+				String x1 = xy[0];
+				String y1 = xy[1];
+				Pair<String, String> start = new Pair<String, String>(x1, y1);
+				if (!paths.containsKey(first)) {
+					paths.put(start, 
+							new ArrayList<Pair<Pair<String, String>, Double>>());
+				}
+				String next = reader.readLine();
+				
+				while (next.startsWith("\t")) {
+					next = next.trim();
+					String tokens[] = next.split(": ");
+					if (tokens.length != 2) {
+						throw new MalformedDataException("Line should have two"
+								+ "coordinates and one distance: " + next);
+					}
+					Double distance = Double.parseDouble(tokens[1]);
+					String[] xy2 = tokens[0].split(",");
+					if (xy2.length != 2) {
+						throw new MalformedDataException("Line should have two"
+								+ "coordinates: " + next);
+					}
+					String x2 = xy2[0];
+					String y2 = xy2[1];
+					Pair<String, String> dest = new Pair<String, String>(x2, y2);
+					Pair<Pair<String, String>, Double> data = 
+							new Pair<Pair<String, String>, Double>(dest, distance);
+					paths.get(first).add(data);
+					next = reader.readLine();
+				}
+				first = next;
+			}
+		} catch (IOException e) {
+			System.err.println(e.toString());
+			e.printStackTrace(System.err);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					System.err.println(e.toString());
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+	}
+}
---
+++ cse331/src/hw8/Location.java
@@ -0,0 +1,97 @@
+package hw8;
+
+/**
+ * <b>Location</b> represents a <b>immutable</b> location that has a short
+ * name, long name, and x and y coordinates.
+ * <p>
+ * 
+ * @author pinyiw
+ */
+public class Location {
+	
+	/** The short name this Location stores. */
+	private final String shortName;
+	/** The long name this Location stores. */
+	private final String longName;
+	/** The x coordinate this Location stores. */
+	private final String x;
+	/** The y coordinate this Location stores. */
+	private final String y;
+	
+	// Abstraction Function:
+	// Location, l, is a location that stores a short name, long name, x, and y.
+	// An example of Location will be (A, B, x1, y1), where A is shortName, B is
+	// longName, x1 is x, and y1 is y.
+	//
+	// Representation invariant for every Location l:
+	//	shortName != null &&
+	//	longName != null &&
+	//	x != null &&
+	//	y != null.
+	
+	/**
+	 * @param shortName the short name of the location to be stored.
+	 * @param longName the long name of the location to be stored.
+	 * @param x the x coordinate of the location to be stored.
+	 * @param y the y coordinate of the location to be stored.
+	 * @effect Constructs a new Location with the given shortName, longName, x
+	 * 		   and y.
+	 * @throws IllegalArgumentException if shortName == null || longName == null
+	 * 		   || x == null || y == null.
+	 */
+	public Location(String shortName, String longName, String x, String y) {
+		if (shortName == null || longName == null || x == null || y == null) {
+			throw new IllegalArgumentException();
+		}
+		this.shortName = shortName;
+		this.longName = longName;
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Returns the short name of this Location.
+	 * 
+	 * @return the short name of this Location.
+	 */
+	public String shortName() {
+		return shortName;
+	}
+	
+	/**
+	 * Returns the long name of this Location.
+	 * 
+	 * @return the long name of this Location.
+	 */
+	public String longName() {
+		return longName;
+	}
+	
+	/**
+	 * Returns the x coordinate of this Location.
+	 * 
+	 * @return x of this Location.
+	 */
+	public String x() {
+		return x;
+	}
+	
+	/**
+	 * Returns the y coordinate of this Location.
+	 * 
+	 * @return y of this Location.
+	 */
+	public String y() {
+		return y;
+	}
+	
+	/**
+	 * Checks that the representation invariant holds.
+	 */
+	public void checkRep() {
+		assert (shortName != null) : "short name is null";
+		assert (longName != null) : "long name is null";
+		assert (x != null) : "x is null";
+		assert (y != null) : "y is null";
+	}
+}
