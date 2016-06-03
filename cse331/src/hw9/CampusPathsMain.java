f5c2f104bc7c3bbe6e0c4d0e0477c4fc49ef0f43
Pinyi Wang <pinyiwang123@gmail.com>
2016-06-02 18:42:50 -0700

    almost finish hw9

--- cse331/src/hw9/CampusPathsMain.java
+++ cse331/src/hw9/CampusPathsMain.java
@@ -39,64 +39,27 @@ public class CampusPathsMain {
 	}
 	
 	private static void init() {
-		try {
-			uwcp = new UWCampusPaths("src/hw8/data/campus_buildings.dat",
-												"src/hw8/data/campus_paths.dat");
-		} catch (MalformedDataException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Map<String, Location> buildings = uwcp.getBuildingsInfo();
+		JComboBox<Object> first = new JComboBox<Object>();
+		JComboBox<Object> second = new JComboBox<Object>();
+		first.addItem("Select Buildings...");
+		second.addItem("Select Buildings...");
 		
-		BufferedImage image = null;
-		try {
-			image = ImageIO.read(new File("src/hw8/data/campus_map.jpg"));
-		} catch (Exception e) {
-			System.out.println("invalid file path");
-		}
-		
-		Double scale = 4.0;
-		UWMap panel = new UWMap(image, scale);
-//		int width = (int)(image.getWidth() / 1);
-//		int height = (int)(image.getHeight() / 1);
-//		System.out.println("Width: " + width + " Height: " + height);
-		// 1082 741
-//		panel.setPreferredSize(new Dimension((int)(width / scale), (int)(height / scale)));
-		
-		String[] builds = new String[buildings.keySet().size()];
-		int count = 0;
-		for (String b: buildings.keySet()) {
-			Location l = buildings.get(b);
-			builds[count] = l.shortName() + ": " + l.longName();
-			count++;
-		}
-		
-		JComboBox<Object> first = new JComboBox<Object>(builds);
-		JComboBox<Object> second = new JComboBox<Object>(builds);
-		
+		UWMap panel = new UWMap(first, second);
+
 		JButton search = new JButton("Find Path");
-		search.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String[] start = ((String)first.getSelectedItem()).split(": ");
-				String[] dest = ((String)second.getSelectedItem()).split(": ");
-				Pair<String, String> startXY = buildings.get(start[0]).xy();
-				Pair<String, String> destXY = buildings.get(dest[0]).xy();
-				GraphNodePath<Pair<String, String>> gnp = 
-										uwcp.findPath(startXY, destXY);
-				int startX = (int)(Double.parseDouble(startXY.getKey()) / scale);
-				int startY = (int)(Double.parseDouble(startXY.getValue()) / scale);
-				int destX = (int)(Double.parseDouble(destXY.getKey()) / scale);
-				int destY = (int)(Double.parseDouble(destXY.getValue()) / scale);
-				panel.setPaintLocation(startX, startY, destX, destY);
-				frame.repaint();
-			}
-		});
+		search.setActionCommand("search");
+		search.addActionListener(panel);
+		
+		JButton reset = new JButton("Reset");
+		reset.setActionCommand("reset");
+		reset.addActionListener(panel);
 		
 		JPanel control = new JPanel();
 		control.setBackground(Color.gray);
 		control.add(first);
 		control.add(second);
 		control.add(search);
+		control.add(reset);
 		control.setLayout(new BoxLayout(control, BoxLayout.Y_AXIS));
 		
 		frame.add(panel, BorderLayout.CENTER);
--- cse331/src/hw9/UWMap.java
+++ cse331/src/hw9/UWMap.java
@@ -4,12 +4,32 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
+import javax.imageio.ImageIO;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
-public class UWMap extends JPanel {
+import hw5.GraphNode;
+import hw7.GraphNodePath;
+import hw8.Location;
+import hw8.Pair;
+import hw8.UWCampusPaths;
+import hw8.DataParser.MalformedDataException;
+
+public class UWMap extends JPanel implements ActionListener {
+	
+	private static final double SCALE = 4.0;
 	private BufferedImage image = null;
+	private GraphNodePath<Pair<String, String>> gnp = null;
+	private JComboBox<Object> first;
+	private JComboBox<Object> second;
+	private UWCampusPaths uwcp;
 	private double scale;
 	private boolean highlight;
 	private int startX;
@@ -21,36 +41,70 @@ public class UWMap extends JPanel {
 	private int maxX;
 	private int maxY;
 	
-	public UWMap(BufferedImage image, double scale) {
-		this.image = image;
-		this.scale = scale;
-		int width = image.getWidth();
-		int height = image.getHeight();
+	public UWMap(JComboBox<Object> first, JComboBox<Object> second) {
+		try {
+			uwcp = new UWCampusPaths("src/hw8/data/campus_buildings.dat",
+					"src/hw8/data/campus_paths.dat");
+		} catch (MalformedDataException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Map<String, Location> buildings = uwcp.getBuildingsInfo();
+		try {
+			image = ImageIO.read(new File("src/hw8/data/campus_map.jpg"));
+		} catch (Exception e) {
+			System.out.println("invalid file path");
+		}
+		this.first = first;
+		this.second = second;
+		for (String b: buildings.keySet()) {
+			Location l = buildings.get(b);
+			first.addItem(l.shortName() + ": " + l.longName());
+			second.addItem(l.shortName() + ": " + l.longName());
+		}
+		this.scale = SCALE;
 		minX = 0;
 		minY = 0;
-		maxX = (int)(width / scale);
-		maxY = (int)(height / scale);
+		maxX = (int)(image.getWidth() / SCALE);
+		maxY = (int)(image.getHeight() / SCALE);
 		highlight = false;
 	}
 	
 	private void paintLocation(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		if (highlight) {
+			g2.setStroke(new BasicStroke(5));
+			g.setColor(Color.red);
+			List<GraphNode<Pair<String, String>>> path = gnp.getPath();
+			Pair<String, String> cur = path.get(0).getName();
+			for (int i = 1; i < path.size(); i++) {
+				Pair<String, String> next = path.get(i).getName();
+				Double curX = Double.parseDouble(cur.getKey()) / SCALE;
+				Double curY = Double.parseDouble(cur.getValue()) / SCALE;
+				Double nextX = Double.parseDouble(next.getKey()) / SCALE;
+				Double nextY = Double.parseDouble(next.getValue()) / SCALE;
+				g2.drawLine(translate(curX, 0, minX), translate(curY, 0, minY), 
+						translate(nextX, 0, minX), translate(nextY, 0, minY));
+				cur = next;
+			}
+			
 			int ovalSize = 20;
 			g2.setColor(Color.yellow);
-			g2.setStroke(new BasicStroke(5));
 //			System.out.println(translate(startX, 10, minX) + " " + (int)(startY / scale - diffY));
 //			System.out.println((int)(destX / scale - diffX) + " " + (int)(destY / scale - diffY));
 			g2.drawOval(translate(startX, 10, minX), translate(startY, 10, minY), ovalSize, ovalSize);
 			g2.drawOval(translate(destX, 10, minX), translate(destY, 10, minY), ovalSize, ovalSize);
+			
 		}
 	}
 	
-	private int translate(int x, int diff, int min) {
+	private int translate(double x, int diff, int min) {
 		return (int)(x / scale - diff + min);
 	}
 	
-	public void setPaintLocation(int startX, int startY, int destX, int destY) {
+	public void setPaintLocation(int startX, int startY, int destX, int destY,
+						GraphNodePath<Pair<String, String>> gnp) {
+		this.gnp = gnp;
 		this.startX = startX;
 		this.startY = startY;
 		this.destX = destX;
@@ -58,19 +112,19 @@ public class UWMap extends JPanel {
 //		System.out.println("(" + startX + ", " + startY + "), (" + destX + ", " + destY + ")");
 		int width = image.getWidth();
 		int height = image.getHeight();
-		minX = - Math.min(startX, destX) - 50;
-		minY = - Math.min(startY, destY) - 50;
+		minX = - Math.min(startX, destX) - 100;
+		minY = - Math.min(startY, destY) - 100;
 		maxX = Math.max(startX, destX) + 50 + minX;
 		maxY = Math.max(startY, destY) + 50 + minY;
-		double scaleX = ((maxX - minX) / ((width / 4.0) - minX));
-		double scaleY = ((maxY - minY) / ((height / 4.0) - minY));
+		double scaleX = ((maxX - minX) / ((width / SCALE) - minX));
+		double scaleY = ((maxY - minY) / ((height / SCALE) - minY));
 		
 //		System.out.println("minX: " + minX + " minY: " + minY);
 //		System.out.println("maxX: " + maxX + " maxY: " + maxY);
 		
 		scale = Math.max(scaleX, scaleY);
-		maxX = (int)(width / 4.0 / scale);
-		maxY = (int)(height / 4.0 / scale);
+		maxX = (int)(width / SCALE / scale);
+		maxY = (int)(height / SCALE / scale);
 //		System.out.println("scaleX: " + scaleX + " scaleY: " + scaleY);
 //		System.out.println("maxX: " + maxX + " maxY: " + maxY);
 		highlight = true;
@@ -81,8 +135,44 @@ public class UWMap extends JPanel {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
 		
-		
 		g2.drawImage(image, minX, minY, maxX, maxY, null);
 		paintLocation(g2);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getActionCommand().equals("search")) {
+			String start = ((String)first.getSelectedItem()).split(": ")[0];
+			String dest = ((String)second.getSelectedItem()).split(": ")[0];
+			String def = "Select Buildings...";
+			if (!start.equals(def) || !dest.equals(def)) {
+				if (start.equals(def)) {
+					start = dest;
+				} else if (dest.equals(def)) {
+					dest = start;
+				}
+				Map<String, Location> buildings = uwcp.getBuildingsInfo();
+				Pair<String, String> startXY = buildings.get(start).xy();
+				Pair<String, String> destXY = buildings.get(dest).xy();
+				GraphNodePath<Pair<String, String>> gnp = 
+						uwcp.findPath(startXY, destXY);
+				int startX = (int)(Double.parseDouble(startXY.getKey()) / SCALE);
+				int startY = (int)(Double.parseDouble(startXY.getValue()) / SCALE);
+				int destX = (int)(Double.parseDouble(destXY.getKey()) / SCALE);
+				int destY = (int)(Double.parseDouble(destXY.getValue()) / SCALE);
+				setPaintLocation(startX, startY, destX, destY, gnp);
+			}
+		} else if (e.getActionCommand().equals("reset")) {
+			this.scale = SCALE;
+			minX = 0;
+			minY = 0;
+			maxX = (int)(image.getWidth() / SCALE);
+			maxY = (int)(image.getHeight() / SCALE);
+			first.setSelectedIndex(0);
+			second.setSelectedIndex(0);
+			highlight = false;
+		}
+		repaint();
 	}
 }
